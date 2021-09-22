77523423cb3b569014d4f02655b705f2bdadd0c2
B4D-1D34 <rapkor4@gmail.com>
2021-09-22 22:10:21 +0500

    content preparations are close to finish, photos are cropped, compressed and delivered to cdn, animations are next

--- src/components/PhotoLayout/PhotoLayout.component.jsx
+++ src/components/PhotoLayout/PhotoLayout.component.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import "./PhotoLayout.styles.css";
+import SOURCE_LINKS from "./photoData";
 
 const PhotoLayout = () => {
   return (
     <div className="PhotoLayout">
-      <div className="image image1">image1</div>
-      <div className="image image2">image2</div>
-      <div className="image image3">image3</div>
-      <div className="image image4">image4</div>
-      <div className="image image5">image5</div>
-      <div className="image image6">image6</div>
-      <div className="image image7">image7</div>
-      <div className="image image8">image8</div>
-      <div className="image image9">image9</div>
-      <div className="image image10">image10</div>
-      <div className="image image11">image11</div>
-      <div className="image image12">image12</div>
+      {SOURCE_LINKS.map((imageUrl, idx) => (
+        <div className={`image image${idx + 1}`}>
+          <img src={imageUrl} alt="Photo"></img>
+        </div>
+      ))}
     </div>
   );
 };
---
+++ src/components/PhotoLayout/photoData.js
@@ -0,0 +1,16 @@
+const SOURCE_LINKS = [
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275355/photographer_assets/figma/compressed/1-min_h4fdno.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275355/photographer_assets/figma/compressed/2-min_u6ktfu.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/3-min_m6cxc6.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/4-min_zhe82t.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/5-min_bbbxus.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/6-min_dt92di.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/7-min_dtdgny.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/8-min_gjteqj.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275356/photographer_assets/figma/compressed/9-min_yp2o1m.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275355/photographer_assets/figma/compressed/10-min_drajqx.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275355/photographer_assets/figma/compressed/11-min_g1bund.jpg",
+  "https://res.cloudinary.com/aparinsite/image/upload/v1632275355/photographer_assets/figma/compressed/12-min_tkwmdm.jpg",
+];
+
+export default SOURCE_LINKS;
